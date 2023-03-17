8bf48d27b48def9c4a4e1961972127612cda6063
arath7 <60801863+arath7@users.noreply.github.com>
2023-03-17 13:01:28 -0700

    Add files via upload

---
+++ htmlProj/arducopterweb/DemoHome/index.php
@@ -0,0 +1,8 @@
+$command = "python /home/user/desktop/BasicArduCopter/Abhi/BasicArdu/arming.py 2>&1";
+
+$python = shell_exec(python3 arming.py);
+echo $python
+
+<?php 
+    echo "Hello folks, I hope XAMPP is working fine!"
+?>
