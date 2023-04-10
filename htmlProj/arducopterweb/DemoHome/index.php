9a41903621c124a851cd021aea7b591420d03278
arath7 <60801863+arath7@users.noreply.github.com>
2023-04-10 12:41:29 -0700

    Delete index.php

--- htmlProj/arducopterweb/DemoHome/index.php
+++
@@ -1,8 +0,0 @@
-$command = "python /home/user/desktop/BasicArduCopter/Abhi/BasicArdu/arming.py 2>&1";
-
-$python = shell_exec(python3 arming.py);
-echo $python
-
-<?php 
-    echo "Hello folks, I hope XAMPP is working fine!"
-?>
